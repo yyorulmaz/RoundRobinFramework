9da57348aeaf6fb81ee7df615165b3f3921a5e67
yusuf yorulmaz <yusufyorulmaz@gmail.com>
2022-09-04 04:07:30 +0300

    round robin framework start

---
+++ src/RoundRobinFramework/IRoundRobin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RoundRobinFramework
+{
+    public interface IRoundRobin<T>
+    {
+        RoundRobinModel<T> Get(Guid id);
+        List<RoundRobinModel<T>> GetAll();
+        RoundRobinModel<T> GetEndRoundRobin();
+        T GetEndRoundRobinData();
+        RoundRobinModel<T> Add(T value, int weight);
+        RoundRobinModel<T> Remove(Guid id);
+        T Next { get; }
+    }
+}
